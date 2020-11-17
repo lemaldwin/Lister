fa45efee803734aa92a8fdb68bd45550bccd73a5
lemaldwin <lemuelaldwin@yahoo.com.ph>
2020-11-18 05:11:01 +0800

    Updated the code for numberedViewer.bash

--- numberedViewer.bash
+++ numberedViewer.bash
@@ -1,4 +1,30 @@
 #!/bin/bash
 
 
-nl -s ") " <list.txt | pr -3 -t
+
+LIST=( `cat list.txt | tr "\n" " "` )
+
+
+echo -e "This is a list of items:"
+nl -s ") " <list.txt | pr -4 -t
+
+
+
+#echo -n "Select an item on the list: "
+read -p "Select an item: " CHOICE
+
+(( CHOICE-- ))
+case "$CHOICE" in
+	0) 
+		echo "This is the lore of ${LIST[$CHOICE]}."
+		;;
+	1)
+		echo "This is the lore of ${LIST[$CHOICE]}."
+		;;
+	*)
+		echo "This is the lore of "
+
+esac
+
+echo -e "\n\n\n"
+
